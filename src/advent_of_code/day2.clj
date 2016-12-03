d7eaf8124b61e09b782907aa3b1c8486af2f2fef
Corey Beres <corey.beres@gmail.com>
2016-12-02 22:34:22 -0500

    day 2 part 2

--- src/advent_of_code/day2.clj
+++ src/advent_of_code/day2.clj
@@ -2,15 +2,19 @@
   (:use [clojure.string :only [split-lines]]))
 
 (def next-key
-  {1 {:up 1, :down 4, :left 1, :right 2}
-   2 {:up 2, :down 5, :left 1, :right 3}
-   3 {:up 3, :down 6, :left 2, :right 3}
-   4 {:up 1, :down 7, :left 4, :right 5}
-   5 {:up 2, :down 8, :left 4, :right 6}
-   6 {:up 3, :down 9, :left 5, :right 6}
-   7 {:up 4, :down 7, :left 7, :right 8}
-   8 {:up 5, :down 8, :left 7, :right 9}
-   9 {:up 6, :down 9, :left 8, :right 9}})
+  {\1 {:up \1, :down \3, :left \1, :right \1}
+   \2 {:up \2, :down \6, :left \2, :right \3}
+   \3 {:up \1, :down \7, :left \2, :right \4}
+   \4 {:up \4, :down \8, :left \3, :right \4}
+   \5 {:up \5, :down \5, :left \5, :right \6}
+   \6 {:up \2, :down \A, :left \5, :right \7}
+   \7 {:up \3, :down \B, :left \6, :right \8}
+   \8 {:up \4, :down \C, :left \7, :right \9}
+   \9 {:up \9, :down \9, :left \8, :right \9}
+   \A {:up \6, :down \A, :left \A, :right \B}
+   \B {:up \7, :down \D, :left \A, :right \C}
+   \C {:up \8, :down \C, :left \B, :right \C}
+   \D {:up \B, :down \D, :left \D, :right \D}})
 
 (defn get-direction [letter]
   (case letter
@@ -28,11 +32,9 @@
 
 (defn password [file]
   (loop [lines (split-lines (slurp file))
-         start 5
+         start \5
          combo []]
     (if (seq lines)
       (let [next-key (get-key start (first lines))]
         (recur (rest lines) next-key (conj combo next-key)))
-      combo)))
-      
-    
+      (apply str combo))))
