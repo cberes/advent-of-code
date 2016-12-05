4226e2d6ba4010031353bfb335bc0ba4101465c1
Corey Beres <corey.beres@gmail.com>
2016-12-05 00:57:18 -0500

    day 5 part 2

--- src/advent_of_code/day5.clj
+++ src/advent_of_code/day5.clj
@@ -1,16 +1,46 @@
 (ns advent-of-code.day5
-  (:use [clojure.string :only [starts-with? trim]])
+  (:use [clojure.string :only [includes? starts-with? trim]])
   (:use [digest :only [md5]]))
+
+(def password-length 8)
+(def zeroes 5)
+(def n-zeroes (apply str (take zeroes (repeat "0"))))
+(def position-index zeroes)
+
+(defn filter-unique-positions
+  ([pairs]
+    (filter-unique-positions nil pairs))
+  ([to-keep pairs]
+    (let [positions (set (map first to-keep))]
+      (if (and (seq pairs) (< (count positions) password-length))
+        (if (contains? positions (first (first pairs)))
+          (filter-unique-positions to-keep (rest pairs))
+          (filter-unique-positions (conj to-keep (first pairs)) (rest pairs)))
+        to-keep))))
+
+(defn to-pair [s]
+  (let [char-index (inc position-index)
+        position-char (subs s position-index char-index)
+        password-char (nth s char-index)]
+    [(Integer/parseInt position-char) password-char]))
 
 (defn hashes [prefix]
   (map #(md5 (str prefix %)) (range (Integer/MAX_VALUE))))
-  
-(defn run [file]
+
+(defn possible-position-pairs [file]
   (->> file
     (slurp)
     (trim)
     (hashes)
-    (filter #(starts-with? % "00000"))
-    (take 8)
-    (map #(nth % 5))
+    (filter #(starts-with? % n-zeroes))
+    (filter #(Character/isDigit (nth % position-index)))
+    (map to-pair)
+    (filter #(< (first %) password-length))))
+
+(defn run [file]
+  (->> file
+    (possible-position-pairs)
+    (filter-unique-positions)
+    (sort #(- (first %1) (first %2)))
+    (map second)
     (apply str)))
