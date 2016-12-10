271eba139030642dab937620c62561ee983a720b
Corey Beres <corey.beres@gmail.com>
2016-12-10 12:37:34 -0500

    day 9 part 2

--- src/advent_of_code/day9.clj
+++ src/advent_of_code/day9.clj
@@ -1,29 +1,31 @@
 (ns advent-of-code.day9
   (:use [clojure.string :only [blank? split-lines trim]]))
 
-(def compression-pattern #"(.*?)(\(\d+x\d+\))")
+(def compression-pattern #"(\w*)(\(\d+x\d+\))?")
 
 (def repeat-pattern #"\((\d+)x(\d+)\)")
+
+(defn find-repetition [s]
+  (rest (re-find compression-pattern s)))
 
 (defn parse-repeat [s]
   (let [[letters times] (rest (re-find repeat-pattern s))]
     [(Integer/parseInt letters) (Integer/parseInt times)]))
 
-(defn do-decompress [[letters times] s]
-  (let [to-repeat (subs s 0 letters)
-        next (subs s letters)]
-    [(apply str (repeat times to-repeat)) next]))
+(defn do-decompress [letters s]
+  [(subs s 0 letters) (subs s letters)])
 
 (defn decompress
-  ([s] (decompress nil s))
+  ([s] (decompress 0 s))
   ([result s]
-    (let [match (re-find compression-pattern s)]
-      (if (blank? (second match))
-        (if (blank? (last match))
+    (let [[text repetition] (find-repetition s)]
+      (if (blank? text)
+        (if (blank? repetition)
           result
-          (let [[insert next] (do-decompress (parse-repeat (last match)) (subs s (count (last match))))]
-            (decompress (str result insert) next)))
-        (decompress (str result (second match)) (subs s (count (second match))))))))
+          (let [[letters times] (parse-repeat repetition)
+                [insert next] (do-decompress letters (subs s (count repetition)))]
+            (decompress (+ result (* times (decompress insert))) next)))
+        (decompress (+ result (count text)) (subs s (count text)))))))
 
 (defn read-lines [file]
   (->> file
@@ -35,5 +37,4 @@
   (->> file
     (read-lines)
     (first)
-    (decompress)
-    (count)))
+    (decompress)))
--- test/advent_of_code/core_test.clj
+++ test/advent_of_code/core_test.clj
@@ -1,6 +1,7 @@
 (ns advent-of-code.core-test
   (:use [clojure.test :refer :all])
-  (:use [advent-of-code.day7 :only [get-all-aba get-all-abba]]))
+  (:use [advent-of-code.day7 :only [get-all-aba get-all-abba]])
+  (:use [advent-of-code.day9 :only [find-repetition]]))
 
 (deftest abba-test
   (testing "day 7"
@@ -8,3 +9,11 @@
     (is (= (get-all-aba "wbwnwn") ["wbw" "wnw" "nwn"]))
     (is (= (get-all-abba "aawbbwnnwee") ["wbbw" "wnnw"]))
     (is (= (get-all-abba "wbbwnnw") ["wbbw" "wnnw"]))))
+
+(deftest compression-test
+  (testing "day 9"
+    (is (= (find-repetition "abc") ["abc" nil]))
+    (is (= (find-repetition "abc(50x51)") ["abc" "(50x51)"]))
+    (is (= (find-repetition "(50x51)abc") ["" "(50x51)"]))
+    (is (= (find-repetition "abc(50x51)def") ["abc" "(50x51)"]))
+    (is (= (find-repetition "[[]//abc") ["" nil]))))
