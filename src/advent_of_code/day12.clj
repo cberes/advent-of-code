c56dbaf34fe9fa56e394f4b324291604ba7eaf38
Corey Beres <corey.beres@gmail.com>
2016-12-13 21:17:24 -0500

    day 12 part 1

---
+++ src/advent_of_code/day12.clj
@@ -0,0 +1,49 @@
+(ns advent-of-code.day12
+  (:use [clojure.string :only [split]])
+  (:use [advent-of-code.util :only [read-lines enumerate to-int]]))
+
+(def initial-state {})
+
+(defn parse-value [s]
+  (try
+    (to-int s)
+    (catch NumberFormatException _ s)))
+
+(defn parse-command [s]
+  (let [tokens (split s #"\s+")]
+    (case (first tokens)
+      "inc" [:inc (second tokens)]
+      "dec" [:dec (second tokens)]
+      "jnz" [:jnz (parse-value (second tokens)) (to-int (last tokens))]
+      "cpy" [:cpy (parse-value (second tokens)) (last tokens)])))
+
+(defn get-register [state register]
+  (or (state register) 0))
+
+(defn get-jnz-offset [state [op value offset]]
+  (if (not= 0 (if (integer? value) value (get-register state value)))
+    offset
+    1))
+
+(defn get-value [state source]
+  (if (integer? source)
+    source
+    (get-register state source)))
+
+(defn process [commands]
+  (loop [index 0
+         state initial-state]
+    (if (< index (count commands))
+      (let [command (nth commands index)]
+        (case (first command)
+          :inc (recur (inc index) (assoc state (second command) (inc (get-value state (second command)))))
+          :dec (recur (inc index) (assoc state (second command) (dec (get-value state (second command)))))
+          :jnz (recur (+ index (get-jnz-offset state command)) state)
+          :cpy (recur (inc index) (assoc state (last command) (get-value state (second command))))))
+      state)))
+
+(defn run [file]
+  (->> file
+    (read-lines)
+    (map parse-command)
+    (process)))
