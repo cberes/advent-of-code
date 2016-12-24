d62c444f1e5d81fb9ae49a978282c38f0a3ef2bf
Corey Beres <corey.beres@gmail.com>
2016-12-24 15:47:23 -0500

    day 22 part 2

--- src/advent_of_code/day22.clj
+++ src/advent_of_code/day22.clj
@@ -35,8 +35,55 @@
        (filter #(< used (:avail %)))
        (map (fn [other] [name (:node other)]))))))
 
+(defn size [nodes]
+  [(->> nodes
+     (map :coords)
+     (map first)
+     (apply max))
+   (->> nodes
+     (map :coords)
+     (map second)
+     (apply max))])
+
+(defn goal-position [nodes]
+  [(first (size nodes)) 0])
+
+(defn find-at [position nodes]
+  (->> nodes
+    (filter #(= (:coords %) position))
+    (first)))
+
+(defn find-by-name [name nodes]
+  (->> nodes
+    (filter #(= (:name %) name))
+    (first)))
+
+(defn move-node [{source-name :node source-avail :avail source-used :used}
+                 {dest-name :node dest-avail :avail dest-used :used}
+                 node]
+  (-> node
+    (assoc :used (cond (= (:node node) source-name) 0
+                       (= (:node node) dest-name) (+ dest-used source-used)
+                       :else (:used node)))
+    (assoc :avail (cond (= (:node node) source-name) (+ source-avail source-used)
+                        (= (:node node) dest-name) (- dest-avail source-used)
+                        :else (:avail node)))))
+
+(defn move [source dest nodes]
+  (map (partial move-node source dest) nodes))
+
+(defn solve
+  ([nodes] (solve (find-at (goal-position nodes) nodes) nodes))
+  ([goal nodes]
+   (println goal)
+   (let [new-goal (find-by-name (second (viable-pairs goal nodes)) nodes)
+         move-goal (partial move goal new-goal)]
+     (if (= (:coords goal) [0 0])
+       goal
+       (solve (first (move-goal [new-goal]))
+              (move-goal nodes))))))
+
 (defn run [file]
   (->> file
     (parse-nodes)
-    (viable-pairs)
-    (count)))
+    (solve)))
